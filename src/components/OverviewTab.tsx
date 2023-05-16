fc6c7cb287a3230d43a46957a7984d4bc81c3939
M J <mohamedmujtaba0210@gmail.com>
2023-05-16 15:12:48 +0200

    First commit

---
+++ src/components/OverviewTab.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
+import StatusCard from "./StatusCard";
+import { Activity, CreditCard, DollarSign, Users } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { RecentSales } from "./dashboardComponents/recent-sales";
+import { Overview } from "./dashboardComponents/overview";
+
+const OverviewTab = () => {
+  return (
+    <Tabs defaultValue="overview" className="space-y-4">
+      <TabsList>
+        <TabsTrigger value="overview">Overview</TabsTrigger>
+        <TabsTrigger value="analytics" disabled>
+          Analytics
+        </TabsTrigger>
+        <TabsTrigger value="reports" disabled>
+          Reports
+        </TabsTrigger>
+        <TabsTrigger value="notifications" disabled>
+          Notifications
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="overview" className="space-y-4">
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+          <StatusCard
+            Icon={DollarSign}
+            title="Total Revenue"
+            content="$45,231.89"
+            des=" +20.1% from last month"
+          />
+          <StatusCard
+            Icon={Users}
+            title="Subscriptions"
+            content="+2350"
+            des="+180.1% from last month"
+          />
+          <StatusCard
+            Icon={CreditCard}
+            title="Sales"
+            content="+12,234"
+            des="+19% from last month"
+          />
+          <StatusCard
+            Icon={Activity}
+            title="Active Now"
+            content="+573"
+            des="+19% from last month"
+          />
+        </div>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+          <Card className="col-span-4">
+            <CardHeader>
+              <CardTitle>Overview</CardTitle>
+            </CardHeader>
+            <CardContent className="pl-2">
+              <Overview />
+            </CardContent>
+          </Card>
+          <Card className="col-span-3">
+            <CardHeader>
+              <CardTitle>Recent Sales</CardTitle>
+              <CardDescription>You made 265 sales this month.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <RecentSales />
+            </CardContent>
+          </Card>
+        </div>
+      </TabsContent>
+    </Tabs>
+  );
+};
+
+export default OverviewTab;
---
+++ src/components/StatusCard.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { DollarSign } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface StatusCardProps {
+  title: string;
+  content: string;
+  des?: string;
+  Icon: React.JSXElementConstructor<i>;
+}
+
+interface i {
+  className: string;
+}
+
+const StatusCard: React.FC<StatusCardProps> = ({
+  title,
+  content,
+  des,
+  Icon,
+}) => {
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">{title}</CardTitle>
+        <Icon className={cn("h-4 w-4 text-muted-foreground")} />
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">{content}</div>
+        <p className="text-xs text-muted-foreground">{des}</p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default StatusCard;
